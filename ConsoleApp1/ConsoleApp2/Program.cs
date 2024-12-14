70f86483a9c18697d7f93471178bdc5b2603f223
kola2424 <kola243212@gmail.com>
2024-12-14 03:02:56 +0200

    dz12

---
+++ ConsoleApp1/ConsoleApp2/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace ConsoleApp2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int fibonachi = 0;
+            int.TryParse(Console.ReadLine(), out fibonachi);
+            int endFibonachi = Fibonachi(fibonachi);
+            Console.WriteLine($"Число фибоначи ={endFibonachi} пoсле {fibonachi} раз");
+        }
+        static int Fibonachi(int number)
+        {
+            if (number <= 1)
+                return number;
+
+            int prev = 0, next = 1;
+            for (int i = 2; i < number; i++)
+            {
+                int temp = prev + next;
+                prev = next;
+                next = temp;
+            }
+            return next;
+        }
+    }
+}
