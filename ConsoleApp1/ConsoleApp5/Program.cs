18f0e9e0881087d4bddb4618c06511ff2cdfe925
kola2424 <kola243212@gmail.com>
2024-12-29 12:21:18 +0200

    Update Program.cs

--- ConsoleApp1/ConsoleApp5/Program.cs
+++ ConsoleApp1/ConsoleApp5/Program.cs
@@ -4,7 +4,6 @@
     {
         static void Main(string[] args)
         {
-            int blok = 0;
             int number = 0;
             string[] game =  { "кот", "мячик", "камень", "футбол", "лошадь" };
             Random random = new Random();
@@ -15,7 +14,6 @@
                 Console.WriteLine("Введите букву");
                 string lit = Console.ReadLine();
                 char lit1 = lit[0];
-
                 if (Game(word, word.Length, lit1) == 0)
                 {
                     Console.WriteLine($"Осталось попыток : {word.Length - i-1}");
@@ -23,7 +21,6 @@
                 }
                 else
                 {
-                    
                     number++;
                 }
                 if (number == word.Length)
@@ -40,18 +37,13 @@
 
             static int Game(string secret, int num, char lit1)
             {
-
-
                 for (int i = 0; i < secret.Length; i++)
                 {
                     if (secret[i] == lit1)
                     {
                         Console.WriteLine($"Вы угадали букву под номером : {i+1} ");
                         return 1;
-                        
                     }
-
-
                 }
                 return 0;
             }
