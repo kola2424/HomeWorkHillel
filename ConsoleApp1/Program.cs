e25ae257aea4f35b6a1f76d0c68a0f2d655aeb2a
kola2424 <kola243212@gmail.com>
2024-12-31 13:39:40 +0200

    list add

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,10 +1,42 @@
-﻿namespace ConsoleApp6
+namespace ConsoleApp2
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            // создания списка и ввод дел
+            List<string> list = new List<string>();
+
+            Console.WriteLine("Введите в список свои дела");
+            for (int i = 0; i < 10; i++)
+            {
+                Console.WriteLine($"Введите своё дело под номером {i + 1}");
+                list.Add(Console.ReadLine());
+            }
+            for (int i = 0; i < list.Count; i++) //  Вывод дел
+            {
+
+                Console.WriteLine($"Дело под номером{i + 1} {list[i]}");
+            }
+            Console.WriteLine("Введите номер дела которое вы сделали");// отметка на выполнения задания
+            int deletenum = 0;
+            int.TryParse(Console.ReadLine(), out deletenum);
+            list[deletenum - 1] = list[deletenum -1 ] + " Выполнено";
+            for (int i = 0; i < list.Count; i++) //  Вывод дел
+            {
+                Console.WriteLine($"Дело под номером{i + 1} {list[i]}");
+            }
+            Console.WriteLine("Введите номер дела которое хотите удалить "); // удаление обьекта 
+            deletenum = 0;
+            int.TryParse(Console.ReadLine(), out deletenum);
+            string temp2 = list[deletenum - 1];
+            list.Remove(temp2);
+            for (int i = 0; i < list.Count; i++) //  Вывод дел 
+            {
+
+                Console.WriteLine($"Дело под номером{i + 1} {list[i]}");
+            }
+
         }
     }
 }
